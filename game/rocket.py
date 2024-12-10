7e08890015a937eaa55d80f907f1177271a0da97
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-12-10 18:56:09 +0100

    describe Rocket class and its methods, add 'scale' field, remove fixing 'y' axis in render method `scale` field is used in render method, it defines how big we want the object section with fixing the 'y' axis was redundant and doing nothing

--- game/rocket.py
+++ game/rocket.py
@@ -19,17 +19,27 @@ class Rocket:
 
         self.angle: float = 0  # Kąt obrotu w stopniach
 
+        self.scale = 3.0
+
     def add_force(self, force: Vector2):
+        """Adds acceleration to the rocket based on the given force"""
         self.acc += force / self.mass
 
-    def rotate(self, direction: int):
-        self.angle += direction
+    def rotate(self, angle: int):
+        """Rotates the rocket by the given angle"""
+        self.angle += angle
         if self.angle > 180:
             self.angle = -(360 - self.angle)
         elif self.angle < -180:
             self.angle = (360 - self.angle)
 
     def accelerate(self, dt: float, accelerating: bool = True):
+        """
+        Accelerates the rocket in the direction it is facing
+        :param dt: time delta from the last frame
+        :param accelerating: if False, the rocket will decelerate
+        :return:
+        """
         thrust_vector: Vector2 = Vector2(
             math.cos(math.radians(self.angle)),
             math.sin(math.radians(self.angle))
@@ -38,21 +48,23 @@ class Rocket:
         self.add_force(thrust_vector)
 
     def handle_events(self, dt):
+        """Handles key presses"""
         pressed = pygame.key.get_pressed()
-        if pressed[pygame.K_w]:
+        if pressed[pygame.K_w]: # moving forward
             self.accelerate(dt)
-        if pressed[pygame.K_s]:
+        if pressed[pygame.K_s]: # moving backward
             self.accelerate(dt, False)
-        if pressed[pygame.K_d]:
+        if pressed[pygame.K_d]: # turning right
             self.rotate(2)
-        if pressed[pygame.K_a]:
+        if pressed[pygame.K_a]: # turning left
             self.rotate(-2)
-        if pressed[pygame.K_UP]:
+        if pressed[pygame.K_UP]: # increasing power
             self.power += 10 if self.power < 10000 else 0
-        if pressed[pygame.K_DOWN]:
+        if pressed[pygame.K_DOWN]: # decreasing power
             self.power -= 10 if self.power > 0 else 0
 
     def update(self, dt):
+        """Updates the rocket's position and velocity"""
         self.handle_events(dt)
         self.vel += self.acc * dt  # Update velocity based on acceleration
         self.pos += self.vel  # Update position based on velocity
@@ -60,18 +72,15 @@ class Rocket:
         self.vel *= self.slip
 
     def render(self, screen):
-        # Base triangle
+        """Renders the rocket on the screen"""
+        # Base shape
         points = [Vector2(10, 0), Vector2(-5, -5), Vector2(-2, 0), Vector2(-5, 5)]
+
         # Rotate points
-
         points = [p.rotate(self.angle) for p in points]
-        # for point in points: point.rotate_ip(angle)
-
-        # Fix y-axis
-        points = [Vector2(p.x, p.y) for p in points]
 
         # Add current position
-        points = [self.pos + p * 3 for p in points]
+        points = [self.pos + p * self.scale for p in points]
 
         # Draw triangle
         pygame.draw.polygon(screen, (250, 50, 5), points)
